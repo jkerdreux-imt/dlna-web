a079b3f41554ac8ecb629c5fc57346b2de2ed143
jkerdreux-imt <jerome.kerdreux@imt-atlantique.fr>
2025-05-25 02:44:59 +0200

    Install all stuff w/ the install script.

--- docker/dlna-web/files/install.sh
+++ docker/dlna-web/files/install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+echo "Install Python 3.8 w/ uv"
+pipx install uv
+~/.local/bin/uv python install 3.8
+~/.local/bin/uv python pin 3.8
 
+echo "Install dlna-web"
 git clone https://github.com/jkerdreux-imt/dlna-web.git /opt/dlna-web
 cd /opt/dlna-web/
 
-/root/.local/bin/uv venv --seed
+~/.local/bin/uv venv --seed
 source .venv/bin/activate
 
 pip install -r /opt/dlna-web/requirements.txt
