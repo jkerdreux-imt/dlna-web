06c13a422e0528ddbd00736880ef517c70a7a8f7
jkerdreux-imt <jerome.kerdreux@imt-atlantique.fr>
2025-05-25 02:25:27 +0200

    Added SERVER_PATH to compose.yaml

--- docker/dlna-web/files/run.sh
+++ docker/dlna-web/files/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "Starting DLNA-web Server"
+echo "Starting DLNA-web Server \"$SERVER_PATH\" "
 
 cd /opt/dlna-web/
+echo "SERVER_PATH = \"$SERVER_PATH\"" > src/config.py
+
 source .venv/bin/activate
 python src/app.py
 
