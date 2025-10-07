56da7d7ebd96ef562b9501a145688c1e52a5af9c
jkerdreux-imt <jerome.kerdreux@imt-atlantique.fr>
2025-10-07 21:46:53 +0200

    Added trap

--- docker-test.sh
+++ docker-test.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 IMAGE_NAME="ghcr.io/jkerdreux-imt/dlna-web"
+CONTAINER_NAME="dlna-web-test"
+
+# Clean up function
+cleanup() {
+    echo "Stopping container..."
+    docker stop $CONTAINER_NAME >/dev/null 2>&1
+    docker rm $CONTAINER_NAME >/dev/null 2>&1
+    exit 0
+}
+
+# Trap Ctrl+C
+trap cleanup INT
 
 echo "Building Docker image..."
 docker build -f ./docker/dlna-web/Dockerfile -t $IMAGE_NAME .
 
 echo "Testing service..."
 docker run --rm \
+    --name $CONTAINER_NAME \
     --network host \
     -e SERVER_PATH="/data: minidlna/" \
     $IMAGE_NAME
